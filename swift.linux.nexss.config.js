4a1b5c6b2eb12a5078f56300d0437356e530739b
Marcin Polak <mapoart@gmail.com>
2020-09-06 19:09:15 +0200

    better installer +sudo

--- swift.linux.nexss.config.js
+++ swift.linux.nexss.config.js
@@ -14,16 +14,21 @@ languageConfig.printCommandLine = "";
 languageConfig.checkSyntax = "";
 languageConfig.interactiveShell = "";
 languageConfig.builders = {};
+
+let sudo = "sudo ";
+if (process.getuid && process.getuid() === 0) {
+  sudo = "";
+}
 languageConfig.compilers = {
   apt: {
     // Ubuntu
-    install: `apt install clang libcurl3 libpython2.7 libpython2.7-dev
+    install: `${sudo}apt install clang libcurl3 libpython2.7 libpython2.7-dev
   wget https://swift.org/builds/swift-5.2.5-release/ubuntu1804/swift-5.2.5-RELEASE/swift-5.2.5-RELEASE-ubuntu18.04.tar.gz
   tar xzf swift-5.2.5-RELEASE-ubuntu18.04.tar.gz
-  sudo mv swift-5.2.5-RELEASE-ubuntu18.04 /usr/share/swift
+  ${sudo}mv swift-5.2.5-RELEASE-ubuntu18.04 /usr/share/swift
   echo "export PATH=/usr/share/swift/usr/bin:$PATH" >> ~/.bashrc
   rm swift-5.2.5-RELEASE-ubuntu18.04.tar.gz
-  apt remove libc6-dbg # This needs to be here due to error: ld-2.27.so 0x7fffffff0005c587: adding range [0x1469a-0x1470a) which has a base that is less than the function's low PC 0x14e10
+  ${sudo}apt remove libc6-dbg # This needs to be here due to error: ld-2.27.so 0x7fffffff0005c587: adding range [0x1469a-0x1470a) which has a base that is less than the function's low PC 0x14e10
   . ~/.bashrc`, // Not working yet on Windows
     command: "swift",
     args: "<file>",
